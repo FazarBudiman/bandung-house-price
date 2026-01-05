0d0fd7e774cdfa6e42cb84a67671985fd3dde636
FazarBudiman <fazarbudiman1907@gmail.com>
2026-01-05 07:19:34 +0700

    add-dagshub-config

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -38,7 +38,7 @@ if __name__ == "__main__":
 
         mlflow.sklearn.log_model(
             sk_model=model,
-            name='model',
+            artifact_path='model',
             input_example=input_example
         )
 
